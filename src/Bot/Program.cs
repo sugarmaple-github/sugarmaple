71c57cef01f1be256621c1ec33cf91cd01d7b309
sugarmaple <sugarmaplenamu@naver.com>
2024-06-09 20:06:28 +0900

    vs diff prepare

--- src/Bot/Program.cs
+++ src/Bot/Program.cs
@@ -1,9 +1,30 @@
-﻿using Sugarmaple.Bot.CommandLine;
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.OLE.Interop;
+using Sugarmaple.Bot.CommandLine;
 using System.CommandLine;
+using System.Runtime.InteropServices;
 
 Console.WriteLine("[C]ontinue/[R]estart/[E]xit");
 const string taskFile = "startTask.json";
 string inputStr;
+
+foreach (var dte in DTEHandler.GetInstances())
+{
+    if (dte != null)
+    {
+        dte.MainWindow.Activate();
+        string older = "", newer = "";
+        dte.ExecuteCommand("Tools.DiffFiles",
+            $"\"{older}\" \"{newer}\"");
+
+        Console.WriteLine("Visual Studio 인스턴스를 성공적으로 제어했습니다.");
+    }
+    else
+    {
+        Console.WriteLine("Visual Studio 인스턴스를 찾을 수 없습니다.");
+    }
+}
 
 var rootCmd = new MainCommand();
 while (true)
@@ -32,3 +53,43 @@ while (true)
 
 rootCmd.InvokeSeveral(inputStr);
 Console.WriteLine("모든 작업이 종료되었습니다.");
+
+public class DTEHandler
+{
+    public static IEnumerable<DTE> GetInstances()
+    {
+        IRunningObjectTable rot;
+        IEnumMoniker enumMoniker;
+        int retVal = GetRunningObjectTable(0, out rot);
+
+        if (retVal == 0)
+        {
+            rot.EnumRunning(out enumMoniker);
+
+            uint fetched = uint.MinValue;
+            IMoniker[] moniker = new IMoniker[1];
+            while (enumMoniker.Next(1, moniker, out fetched) == 0)
+            {
+                IBindCtx bindCtx;
+                CreateBindCtx(0, out bindCtx);
+                string displayName;
+                moniker[0].GetDisplayName(bindCtx, null, out displayName);
+                Console.WriteLine("Display Name: {0}", displayName);
+                bool isVisualStudio = displayName.StartsWith("!VisualStudio");
+                if (isVisualStudio)
+                {
+                    object obj;
+                    rot.GetObject(moniker[0], out obj);
+                    var dte = (DTE)obj;
+                    yield return dte;
+                }
+            }
+        }
+    }
+
+    [DllImport("ole32.dll")]
+    private static extern void CreateBindCtx(int reserved, out IBindCtx ppbc);
+
+    [DllImport("ole32.dll")]
+    private static extern int GetRunningObjectTable(int reserved, out IRunningObjectTable prot);
+}
